37e90604500045bf5bb61d4fc08b890c9435af48
Kamil Tirkey <149951494+kamiltirkey@users.noreply.github.com>
2024-04-21 14:37:27 -0400

    Update Histogram of Tweets.sql

--- SQL _Easy/Histogram of Tweets.sql
+++ SQL _Easy/Histogram of Tweets.sql
@@ -16,21 +16,21 @@ tweets Table:
 |___________________________|
 	
 tweets Example Input:
-|----------------------------------------------------------------------------------------------------------------------| 
-| tweet_id   | user_id	    | msg	                                                               | tweet_date          |
-|____________|______________|____________________________________________________________________|_____________________| 
-| 214252	   | 111	        | Am considering taking Tesla private at $420. Funding secured.      | 12/30/2021 00:00:00 |  
-| 739252	   | 111	        | Despite the constant negative press covfefe	                       | 01/01/2022 00:00:00 |
-| 846402	   | 111	        | Following @NickSinghTech on Twitter changed my life!	             | 02/14/2022 00:00:00 |
-| 241425	   | 254	        | If the salary is so competitive why won’t you tell me what it is?	 | 03/01/2022 00:00:00 |
-| 231574     | 148	        | I no longer have a manager. I can't be managed	                   | 03/23/2022 00:00:00 |
-|____________|______________|____________________________________________________________________|_____________________|
+|------------------------------------------------------------------------------------------------------------------------------| 
+| tweet_id   | user_id	    | msg	                                                                 | tweet_date          |
+|____________|______________|____________________________________________________________________________|_____________________| 
+| 214252     | 111	    | Am considering taking Tesla private at $420. Funding secured.              | 12/30/2021 00:00:00 |  
+| 739252     | 111	    | Despite the constant negative press covfefe	                         | 01/01/2022 00:00:00 |
+| 846402     | 111	    | Following @NickSinghTech on Twitter changed my life!	                 | 02/14/2022 00:00:00 |
+| 241425     | 254	    | If the salary is so competitive why won’t you tell me what it is?	         | 03/01/2022 00:00:00 |
+| 231574     | 148	    | I no longer have a manager. I can't be managed	                         | 03/23/2022 00:00:00 |
+|____________|______________|____________________________________________________________________________|_____________________|
   
 Example Output:
 |----------------------------
 | tweet_bucket	| users_num | 
 |_______________|___________|
-|      1	      |      2    |
+|      1	|      2    |
 |      2        |      1    |   
 |_______________|___________|  
 Explanation:
